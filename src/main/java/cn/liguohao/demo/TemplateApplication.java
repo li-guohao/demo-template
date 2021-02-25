db102c162a798744ae05d1230891ed8a165d53a1
li-guohao <liguohao_cn@qq.com>
2021-02-25 17:02:35 +0800

    basic config

---
+++ src/main/java/cn/liguohao/demo/TemplateApplication.java
@@ -0,0 +1,16 @@
+package cn.liguohao.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:liguohao_cn@qq.com">liguohao_cn@qq.com</a>
+ * @since 2021/2/25
+ */
+@SpringBootApplication
+public class TemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateApplication.class,args);
+    }
+}
